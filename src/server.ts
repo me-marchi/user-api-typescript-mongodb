e98181027ca1b309055ef75f616e28cbc44e8532
me-marchi <me.marchi@outlook.com>
2023-01-11 18:43:02 -0300

    feat: express config

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,11 @@
-console.log('hi')
+import express from 'express';
+import { config } from 'dotenv';
+
+const app = express();
+config();
+
+const port = process.env.PORT || 8000;
+
+app.listen(port, () => console.log(`Server running on port ${port}`));
+
+app.get('/', (req, res) => res.send('Olá'));
